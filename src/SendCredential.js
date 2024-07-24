2721bb5ba5259f21c02809af9b9b5b8a284a3b43
Geoffrey Cuff-Chartrand <geoffrey.cuffchartrand@gmail.com>
2024-07-24 10:54:57 -0400

    Frontend post working

---
+++ src/SendCredential.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+async function sendCred(credential) {
+    return fetch('http://localhost:3001/', {
+        method: 'POST',
+        body: JSON.stringify(credential),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+   }
+
+export default function SaveCredential() {
+    const [credential, setCredential] = useState('');
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        sendCred(credential);
+      }
+
+    return(
+        <div className="login-wrapper">
+          <h1>Please Log In</h1>
+          <form onSubmit={handleSubmit}>
+            <label>
+              <p>Username</p>
+              <input type="text" onChange={e => setCredential(JSON.stringify(e.target.value))} />
+            </label>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+    )
+}

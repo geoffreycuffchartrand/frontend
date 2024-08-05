ed17c81d5b08d4af2acc1eeaddb460b11b5a6ae8
Geoffrey Cuff-Chartrand <geoffrey.cuffchartrand@gmail.com>
2024-08-05 14:47:15 -0400

    wip on query page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,90 @@
 import './App.css';
 import SaveCredential from '../src/SendCredential.js';
 import SearchCredential from '../src/SearchCredential.js';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 function App() {
   
     return(
-      SaveCredential()
+      <div>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="search" element={<SearchPage />} />
+            <Route path="save" element={<SavePage />} />
+
+            {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </div>
+  );
+}
+
+
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/save">Save a Credential</Link>
+          </li>
+          <li>
+            <Link to="/search">Search Credentials</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function SavePage() {
+  return (
+    SaveCredential()
+  );
+}
+
+function SearchPage() {
+  return (
+    SearchCredential()
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
   );
 }
 
---
+++ src/SearchCredential.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+async function searchCred(name) {
+    name = name.slice(1, -1)
+    return fetch(`http://localhost:3001/search?name=${name}`, {
+        mode:"cors"
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}
+
+export default function SearchCredential() {
+    const [credential, setCredential] = useState('');
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        searchCred(credential);
+      }
+
+    return(
+        <div className="login-wrapper">
+          <h1>Please Log In</h1>
+          <form onSubmit={handleSubmit}>
+            <label>
+              <p>Enter name to search for credential</p>
+              <input type="text" onChange={e => setCredential(JSON.stringify(e.target.value))} />
+            </label>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+    )
+}

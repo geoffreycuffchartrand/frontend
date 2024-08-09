aecd1c6c6d7473d0479304aba5f3414dce466419
Gregory Cuff <gregory.cuff@gmail.com>
2024-08-09 13:01:17 +0100

    datagrid example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
-import SaveCredential from '../src/SendCredential.js';
+import TableDisplay from '../src/TableDisplay.js';
+// import SaveCredential from '../src/SendCredential.js';
 // import SearchCredential from '../src/SearchCredential.js';
 
+function App() {
+
+  // create 1000 rows of fake data — the query results from the server will go here instead
+  const creds = ["IB Diploma", "OSSD Diploma", "BA", "PhD", "ARCT"]; // list of some possible credentials
+  let credData = [];
+  for (let i = 1; i<1000; i++) {
+    let randomCred = creds[Math.floor(Math.random() * creds.length)]; // pick random credential from list
+    credData.push( // and stick it onto the end of credData
+      { id: i, credential: randomCred, verified: "Hopefully"}
+      )
+  }
+
+  return(
+    TableDisplay(credData)
+  );
+}
+
+/*
 function App() {
   
     return(
       SaveCredential()
   );
 }
+*/
 
 export default App;

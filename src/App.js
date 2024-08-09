b37a63086a6d096fee80f91745c55670f5290a7e
Gregory Cuff <gregory.cuff@gmail.com>
2024-08-09 13:01:17 +0100

    datagrid example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import TableDisplay from '../src/TableDisplay.js';
 import SaveCredential from '../src/SendCredential.js';
-import SearchCredential from '../src/SearchCredential.js';
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+// import SearchCredential from '../src/SearchCredential.js';
 
+function App() {
+
+  // create 1000 rows of fake data — the query results from the server will go here instead
+  const creds = ["IB Diploma", "OSSD Diploma", "BA", "PhD", "ARCT"]; // list of some possible credentials
+  let credData = [];
+  for (let i = 1; i<1000; i++) {
+    let randomCred = creds[Math.floor(Math.random() * creds.length)]; // pick random credential from list
+    credData.push( // and stick it onto the end of credData
+      { id: i, credential: randomCred, verified: "Hopefully"}
+      )
+  }
+
+  return(
+    TableDisplay(credData)
+  );
+}
+
+/*
 function App() {
   
     return(
--- src/SendCredential.js
+++ src/SendCredential.js
@@ -1,34 +1,41 @@
 import React, { useState } from 'react';
 
-async function sendCred(credential) {
-    return fetch('http://localhost:3001/', {
+/*
+Summary:
+
+The front end page has a textbox for the user to enter their credential.
+When they click submit, their input is sent to the backend server with a fetch(), and the returned data or error are printed to the console.
+*/
+
+async function sendCred(credential) { // sends credential; to be used later when submit button is pressed
+    return fetch('http://localhost:3001/', { // fetch returns a 'Promise' (to supply a value later) - a Promise object is either pending, fulfilled (with a 'Response' object containing the value), or rejected (with a reason)
         mode: "cors",
-        method: 'POST',
-        body: JSON.stringify(credential),
+        method: 'POST', // a POST request submits an entry to a specified server resource, rather than, for example, just requesting something
+        body: JSON.stringify(credential), // in this case, it submits whatever's stored in the credential variable
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json' // tells the server that what's been sent is a json string
         }
     })
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-   }
+    .then(data => console.log(data)) // if/when the promise is fulfilled, return the associated data to the console
+    .catch(error => console.error(error)); // if/when the promise is rejected, return the associated error to the console 
+  }
 
 export default function SaveCredential() {
-    const [credential, setCredential] = useState('');
+    const [credential, setCredential] = useState(''); // create state variable ('credential') and function ('setCredential') to update it and render again; initial value is empty ('')
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-        sendCred(credential);
+    const handleSubmit = async e => { // creates an asynchronous function (one that returns a promise); ie the fetch above
+        e.preventDefault(); 
+        sendCred(credential); // 
       }
 
-    return(
+    return( // update credential state variable to whatever is entered in the text box ('e.target.value'), in JSON notation ('JSON.stringify'), and when user presses submit, call sendCred
         <div className="login-wrapper">
           <h1>Please Log In</h1>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}> 
             <label>
               <p>Username</p>
-              <input type="text" onChange={e => setCredential(JSON.stringify(e.target.value))} />
-            </label>
+              <input type="text" onChange={e => setCredential(JSON.stringify(e.target.value))} />  
+            </label> 
             <div>
               <button type="submit">Submit</button>
             </div>
